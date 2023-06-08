82955db899bbb080917f9004487c4cb2a61c6ef3
Psybernautic <s.posada@hotmail.com>
2023-06-08 13:56:18 -0400

    Database container created
    database container created with persistent volume control. Connection successful to the database container using mongoose

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express()
 
+mongoose.connect("mongodb://sebastian:admin123@172.22.0.3:27017/database?authSource=admin")
+.then(() => console.log("Successfully connected to Database"))
+.catch((e) => console.log(e));
+
 app.get("/",(req, res) =>{
-    res.send("<h2>We are in development!!!!</h2>");
+    res.send("<h2>Changes made</h2>");
 })
 
 const port = process.env.PORT || 3000;
