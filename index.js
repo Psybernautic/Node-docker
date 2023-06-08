0246caa1ca7ebfe54b6ba9f0eaa46f15ad2a6bdc
Psybernautic <s.posada@hotmail.com>
2023-06-08 14:32:24 -0400

    Database connection dependancy
    Node-app container will try to connect to the database container. If the image is not running it will retry the connection every 5 seconds until successful.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASS, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const app = express()
 
-mongoose.connect("mongodb://sebastian:admin123@172.22.0.3:27017/database?authSource=admin")
-.then(() => console.log("Successfully connected to Database"))
-.catch((e) => console.log(e));
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_IP}:${MONGO_PORT}/database?authSource=admin`
+
+const connectWithRetry = () => {
+    mongoose.connect(mongoURL)
+    .then(() => console.log("Successfully connected to database"))
+    .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    });
+}
+
+connectWithRetry()
+
 
 app.get("/",(req, res) =>{
     res.send("<h2>Changes made</h2>");
